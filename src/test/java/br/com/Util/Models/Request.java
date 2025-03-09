9e6675ed3e03d06ffac493c32da158a52dd881cf
leticia.rosa <leticia.rosa@apisul.com.br>
2025-03-09 16:07:06 -0300

    inclusao url

--- src/test/java/br/com/Util/Models/Request.java
+++ src/test/java/br/com/Util/Models/Request.java
@@ -2,4 +2,8 @@ package br.com.Util.Models;
 
 public class Request {
     private final ServeRest buscarToken = new ServeRest();
+    private final String url = "https://serverest.dev/";
+    private String obterToken(){
+        return buscarToken.token();
+    }
 }
