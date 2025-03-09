d74de531a5102db746418efcabac21f1be0c3c14
leticia.rosa <leticia.rosa@apisul.com.br>
2025-03-09 15:49:29 -0300

    inclusao classe para realizar token

---
+++ src/test/java/br/com/Util/Models/ServeRest.java
@@ -0,0 +1,30 @@
+package br.com.Util.Models;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+import static io.restassured.RestAssured.given;
+
+public class ServeRest {
+
+    @Test
+    @DisplayName("GetToken")
+    public String token(){
+        String baseURI = "https://serverest.dev";
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("login,json");
+
+        String token = given()
+                .contentType(ContentType.JSON)
+                .body(inputStream)
+                .when()
+                .post("/login")
+                .then()
+                .extract()
+                .path("accessToken");
+        return token;
+
+    }
+}
